17f0ea3e868acf5edfb8ceaba97ef37fcc1a7314
James Keane <jpkeane@me.com>
2017-09-23 00:34:07 +0100

    Added skeleton data seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p1 = Promotion.create(name: 'Speedworth Motorsports')
+
+t1 = Track.create(name: 'Ipswich Foxhall Stadium', promotion: p1)
