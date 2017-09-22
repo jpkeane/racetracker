cdc2aecc74e5e9b46329ea5bc3fcda2d10516f68
James Keane <jpkeane@me.com>
2017-09-23 00:41:18 +0100

    Reannotated models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@
 #       root GET  /                         front_pages#home
 # promotions GET  /promotions(.:format)     promotions#index
 #  promotion GET  /promotions/:id(.:format) promotions#show
+#     tracks GET  /tracks(.:format)         tracks#index
+#      track GET  /tracks/:id(.:format)     tracks#show
 
 Rails.application.routes.draw do
   root 'front_pages#home'
