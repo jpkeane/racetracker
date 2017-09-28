ed81819e8be10b972f4faf6852262a4ec8853808
James Keane <jpkeane@me.com>
2017-09-28 20:09:35 +0100

    Annotated Models and Routes

--- spec/factories/formula_fixtures.rb
+++ spec/factories/formula_fixtures.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: formula_fixtures
+#
+#  id         :integer          not null, primary key
+#  slug       :string           not null
+#  fixture_id :integer          not null
+#  formula_id :integer          not null
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :formula_fixture do
     fixture
--- spec/models/formula_fixture_spec.rb
+++ spec/models/formula_fixture_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: formula_fixtures
+#
+#  id         :integer          not null, primary key
+#  slug       :string           not null
+#  fixture_id :integer          not null
+#  formula_id :integer          not null
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe FormulaFixture, type: :model do
